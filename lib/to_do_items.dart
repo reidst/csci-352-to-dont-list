884d86cf594c70be84df82134e9187f9ce6e2b76
Simon <reidst@hendrix.edu>
2022-09-03 17:19:56 -0500

    Fixed poorly-formatted list items
    Swapped item descriptions and abbreviations to be in the correct order

--- lib/to_do_items.dart
+++ lib/to_do_items.dart
@@ -59,10 +59,10 @@ class ToDoListItem extends StatelessWidget {
           : null,
       leading: CircleAvatar(
         backgroundColor: _getColor(context),
-        child: Text(item.name),
+        child: Text(item.abbrev()),
       ),
       title: Text(
-        item.abbrev(),
+        item.name,
         style: _getTextStyle(context),
       ),
     );
